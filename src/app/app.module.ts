0cc162443732eb07c584833f20d195c4b5c577d8
SudhaMariappan <sudhamarimuthum@gmail.com>
2021-01-29 22:16:10 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule} from "@angular/material/form-field";
+import { MatCardModule} from '@angular/material/card';
+import { MatInputModule} from '@angular/material/input'
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatSelectModule} from '@angular/material/select';
+import { ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatFormFieldModule,MatCardModule,MatInputModule,MatButtonModule,
+    MatToolbarModule,FlexLayoutModule,MatSelectModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
